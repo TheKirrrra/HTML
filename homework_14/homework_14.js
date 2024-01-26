d21c8963616bd65132986de7e67ad50c35bce5c2
Kirrrra <kirrrra007@gmail.com>
2024-01-26 16:49:34 +0100

    last update homework_14

--- homework_14/homework_14.js
+++ homework_14/homework_14.js
@@ -3,8 +3,7 @@ const auto = document.getElementById("autoInfo")
 
 async function getAuto() {
     const result = await fetch("https://thekirrrra.github.io/homework_14/homework_14.json")
-    const jsonData = await result.json()
-    const data = jsonData.success
+    const data = await result.json()
     console.log(data)
     auto.innerText = `${data.model} ${data.year} ${data.color} ${data.id}`;
 }
